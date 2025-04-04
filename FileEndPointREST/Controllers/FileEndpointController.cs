6c47e8d0d4e31ae1464f36ecd3527325067a3425
Paprika Seidler <pas002@edu.zealand.dk>
2025-04-04 11:47:56 +0200

    Add project files.

---
+++ FileEndPointREST/Controllers/FileEndpointController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FileEndpointRepo;
+
+namespace FileEndPointREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileEndpointController : ControllerBase
+    {
+        private FileEndpointRepository _fileEndpointRepository;
+
+        public FileEndpointController(FileEndpointRepository fileEndpointRepository)
+        {
+            _fileEndpointRepository = fileEndpointRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetFiles()
+        {
+            var files = _fileEndpointRepository.GetFileNames();
+            return Ok(files);
+        }
+    }
+}
